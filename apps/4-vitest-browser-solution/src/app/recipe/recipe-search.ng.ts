2ec339906417aab410cd92a5553c0ef18589906c
Younes Jaaidi <younes@marmicode.io>
2025-09-25 23:51:16 +0200

    test: ✅ add 4-vitest-browser's solution

---
+++ apps/4-vitest-browser-solution/src/app/recipe/recipe-search.ng.ts
@@ -0,0 +1,52 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { Suspense, suspensify } from '@jscutlery/operators';
+import { BehaviorSubject, switchMap } from 'rxjs';
+import { Catalog } from '../shared/catalog.ng';
+import { Recipe } from './recipe';
+import { RecipeFilterCriteria } from './recipe-filter-criteria';
+import { RecipeFilter } from './recipe-filter.ng';
+import { RecipePreview } from './recipe-preview.ng';
+import { RecipeRepository } from './recipe-repository';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { AdBanner } from '../ad/ad-banner.ng';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'wm-recipe-search',
+  imports: [Catalog, RecipeFilter, RecipePreview, MatProgressSpinner, AdBanner],
+
+  template: `
+    <wm-ad-banner />
+    <wm-recipe-filter (filterChange)="filter.set($event)"></wm-recipe-filter>
+    <wm-catalog>
+      @if (recipes.isLoading()) {
+        <mat-progress-spinner mode="indeterminate" />
+      }
+
+      @if (recipes.hasValue()) {
+        @for (recipe of recipes.value(); track recipe.id) {
+          <wm-recipe-preview [recipe]="recipe" />
+        } @empty {
+          <div>No recipes found</div>
+        }
+      }
+    </wm-catalog>
+  `,
+})
+export class RecipeSearch {
+  protected filter = signal<RecipeFilterCriteria>({});
+  protected recipes = rxResource({
+    params: () => this.filter(),
+    stream: ({ params }) => this._recipeRepository.search(params),
+  });
+
+  private _recipeRepository = inject(RecipeRepository);
+}
