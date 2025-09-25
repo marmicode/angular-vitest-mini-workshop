a5b22ea20b879fc4547593aee63b6248a15c95a4
Younes Jaaidi <younes@marmicode.io>
2025-09-25 11:32:30 +0200

    feat: ✨ add whiskmate app

--- apps/whiskmate/src/app/app.config.ts
+++ apps/whiskmate/src/app/app.config.ts
@@ -1,15 +1,9 @@
 import {
   ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
+  provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
-  ],
+  providers: [provideHttpClient(), provideZonelessChangeDetection()],
 };
