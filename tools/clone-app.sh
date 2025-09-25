f1e2874caa30ae0f51e6b621d181f53980298394
Younes Jaaidi <younes@marmicode.io>
2025-09-25 15:26:32 +0200

    build: 📦 add clone-app script

---
+++ tools/clone-app.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <source-app> <destination-app>"
+  exit 1
+fi
+
+SOURCE="$1"
+DESTINATION="$2"
+
+cp -r apps/$SOURCE apps/$DESTINATION
+cd apps/$DESTINATION
+sed -i '' "s/$SOURCE/$DESTINATION/g" project.json
