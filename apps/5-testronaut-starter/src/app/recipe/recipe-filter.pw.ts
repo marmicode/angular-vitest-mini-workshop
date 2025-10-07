b67ddf4b3c0e09e9bbd69abe95f92fa52bcfbbe3
Younes Jaaidi <younes@marmicode.io>
2025-10-07 15:47:18 +0200

    test: ✅ add boilerplate for starter

---
+++ apps/5-testronaut-starter/src/app/recipe/recipe-filter.pw.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@testronaut/angular';
+import { RecipeFilter } from './recipe-filter.ng';
+
+test('recipe filter', async ({ page, mount }) => {
+  test.skip(true, '🚧 Work in progress');
+});
---
+++ apps/5-testronaut-starter/src/app/recipe/recipe-search.pw.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@testronaut/angular';
+import { RecipeSearch } from './recipe-search.ng';
+
+test('recipe search', async ({ page, mount, runInBrowser }) => {
+  test.skip(true, '🚧 Work in progress');
+});
